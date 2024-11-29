ec131c0c23fa30358996a7e502679176d85b6728
小马一只 <luo6812@163.com>
2024-11-29 18:04:31 +0800

    fix admin permission issue

--- build.ps1
+++ build.ps1
@@ -1,3 +1,5 @@
 pyinstaller -w --onefile collector.py
 pyinstaller -w --onefile server.py
-pyinstaller -w --onefile validator.py
+pyinstaller -w --onefile --uac-admin validator.py
+cp .\build\validator\validator.exe.manifest ./
+pyinstaller -w --onefile --uac-admin -r validator.exe.manifest,1 validator.py
